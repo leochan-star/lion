e5bec3a2bdf59be6050d2cb5694fa11571977854
leochan-star <547650646@qq.com>
2020-04-17 17:53:40 +0800

    第二次

--- demo01.py
+++ demo01.py
@@ -5,6 +5,36 @@
 
 
 
-a =float(input('请输入：'))
-b =int(input('请输入：'))
-print('input获取的内容：',a+b)
+# a =float(input('请输入：'))
+# b =int(input('请输入：'))
+# print('input获取的内容：',a+b)
+# a  = (1,2,'嘻嘻','嘻嘻',True,False)
+# print (a.index('嘻嘻'))
+# print (a.count('嘻嘻'))
+# b = (a,2,3)
+# print (b[0][2])
+# print (a[0:4])
+# a = [1,2,'嘻嘻','嘻嘻',True,False]
+# # a.append('append')
+# # print (a)
+# a.insert(2,'insert')
+# print (a)
+# b = a.pop(0)
+# c = a.pop(0)
+# print(c+b)
+# print(b)
+# a = {0:2,3:5,'name':'名字'}
+# print (a['name'])
+# a['身高']='185'
+# print(a)
+# a['name']='乃木'
+# print(a)
+# print(a.pop('name'))
+# a.update(name='乃木')
+# print(a)
+# b=a.pop('name')
+# print(b)
+# del a['name']
+# print(a)
+a = {'name':'阿兰','age':25,'sex':'female'}
+print(a['name'],a['age'],a['sex'])
